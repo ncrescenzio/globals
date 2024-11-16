3c9d99f2e1cd126d45d8a8889a72b5587922c543
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2024-11-16 14:05:02 +0100

    update

--- libsrcs/01modNorms.f90
+++ libsrcs/01modNorms.f90
@@ -6,65 +6,65 @@ module Norms
 
    private
 
-   public :: abs_norm, compute_norm, euc_norm, dir_norm
-
    !>-----------------------------------------------------------
-   !> Abstract type for definition of the
-   !> evaluation procedure of the residum
-   !>----------------------------------------------------------
-   type, abstract :: abs_norm
-      !> General definition of norm of a vector
-      !> fucntion from R^n to [0,+infty[
-      !> Example:
-      !>   \| v \| = sqrt(\sum_{i} (v_i)^2)
-      !>   \| v \| = sqrt(\sum{i \ni Dirichelt} m (v_i)^2)
-      !>   \| v \| = sqrt( V^t G v ) for a metric G
+   !> @brief Abstract type for the definition of the
+   !> procedure for the evaluation of the norm of a vector
+   !>
+   !> @details General definition of norm of a vector function
+   !>
+   !> **Example:**
+   !> * \f$\| v \| = \sqrt{\sum_{i} v_i^2}\f$
+   !> * \f$\| v \| = \sqrt{\sum_{i \ni D}  v_i^2}\f$, where \f$D\f$
+   !>   is a set of indices
+   !> * \f$\| v \| = \sqrt{ v^{\top} G v }\f$, for a metric \f$G\f$
+   type, public, abstract :: abs_norm
    contains
-      !> Abstract Procedure for the evaluation of the norm
-      procedure(compute_norm), deferred :: eval_norm
+      !> Abstract procedure for the evaluation of the norm
+      procedure(compute_norm), public, deferred :: eval_norm
    end type abs_norm
 
    abstract interface
-   !>-------------------------------------------------------------
-   !> Genreal norm interface
-   !<-------------------------------------------------------------
-   function compute_norm(this,nvec,vec,scr) result (norm)
-      use Globals
-      import abs_norm
-      implicit none
-      class(abs_norm),             intent(inout) :: this
-      integer,                     intent(in   ) :: nvec
-      real(kind=double),           intent(in   ) :: vec(nvec)
-      ! TODO remove this optinal arguement and defind the scratch
-      ! vectors in the concrete type???
-      real(kind=double), optional, intent(inout) :: scr(nvec)
-      ! output result
-      real(kind=double) :: norm
-   end function compute_norm
+      !>-----------------------------------------------------------
+      !> @brief Abstract interface for the computation of the norm
+      !> @param[inout] this: Object calling the function
+      !> @param[in]    nvec: Integer, length of the vector
+      !> @param[in]    vec:  Real, input vector
+      !> @param[inout] scr:  optional
+      !> @param[out] result: Norm of the input vector
+      !>----------------------------------------------------------
+      function compute_norm(this,nvec,vec,scr) result (norm)
+         use Globals
+         import abs_norm
+         implicit none
+         class(abs_norm),             intent(inout) :: this
+         integer,                     intent(in   ) :: nvec
+         real(kind=double),           intent(in   ) :: vec(nvec)
+         real(kind=double), optional, intent(inout) :: scr(nvec)
+         ! output result
+         real(kind=double) :: norm
+      end function compute_norm
    end interface
 
-
-   type, extends(abs_norm) :: euc_norm
-      !> Use the classical eucliadian norm
+   !> @brief Euclidean norm
+   type, public, extends(abs_norm) :: euc_norm
    contains
-      !>
-      !> Procedure for \|res\|
+      !> Evaluation of the Euclidean norm
       procedure, public :: eval_norm => eval_euc_norm
    end type euc_norm
 
-   type, extends(abs_norm) :: dir_norm
-      !> Use a weighted version of euclian norm
-      !> where the entries on the dirichlet node are set to zero
-      !> Number of Dirichlet Nodes
-      !> G(i,j) = \delta_{i,j} (1 - \delta{i,idir})
-      !> for idir in noddir
+   !> @brief Weighted version of Euclian norm.
+   !> @details The entries on the Dirichlet node are set to zero
+   !> \f$G_{i,j} = \delta_{i,j} (1 - \delta_{i,k})\f$
+   !> for \f$k\f$ in `noddir`.
+   type, public, extends(abs_norm) :: dir_norm
+      !> Number of Dirichlet nodes
       integer :: ndir
       !> Indeces of the Dirichlet Nodes
       integer, allocatable :: noddir(:)
    contains
-      !> Evaluation procedure
+      !> Constructor for type `dir_norm`
       procedure, public :: init => init_dir_norm
-      !> Evaluation procedure
+      !> Evaluation of the weighted Euclidean norm
       procedure, public :: eval_norm => eval_dir_norm
    end type dir_norm
 
@@ -128,7 +128,7 @@ contains
       else
          scr = vec
          do i=1,this%ndir
-         scr(this%noddir(i)) = zero
+            scr(this%noddir(i)) = zero
          end do
          resnorm = dnrm2(nvec,scr,1)
       end if
