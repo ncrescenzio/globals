12fa72dde2652c4d05c86360622e124f4e6a017d
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2026-03-09 22:39:52 +0100

    wip

--- cmake/jsonfortran.cmake
+++ cmake/jsonfortran.cmake
@@ -2,56 +2,100 @@ set(JSONFORTRAN_LIBRARY "jsonfortran::jsonfortran")
 
 if(NOT TARGET "${JSONFORTRAN_LIBRARY}")
 
-   string(TOLOWER "jsonfortran-${CMAKE_Fortran_COMPILER_ID}" JSONFORTRAN_PACKAGE_NAME)
+   set(JSONFORTRAN_SUBPROJECT "external/json-fortran")
+   set(JSONFORTRAN_SOURCE_DIR "${PROJECT_SOURCE_DIR}/${JSONFORTRAN_SUBPROJECT}")
+   set(JSONFORTRAN_BINARY_DIR "${PROJECT_BINARY_DIR}/${JSONFORTRAN_SUBPROJECT}")
 
-   set(JSONFORTRAN_FROM_GIT ON CACHE BOOL "Get 'jsonfortran' library from Github")
+   set(JSONFORTRAN_ENABLE_DOC_GENERATION OFF)  # disable generation documentation
+   set(JSONFORTRAN_ENABLE_TESTS OFF)           # disable tests
+   set(JSONFORTRAN_STATIC_LIBRARY_ONLY ON)     # build only static library
+   add_subdirectory(
+      ${JSONFORTRAN_SOURCE_DIR}
+      ${JSONFORTRAN_BINARY_DIR}
+      EXCLUDE_FROM_ALL
+   )
 
-   if (DEFINED JSONFORTRAN_ROOT)
-      set(${JSONFORTRAN_PACKAGE_NAME}_ROOT "${JSONFORTRAN_ROOT}" CACHE STRING "jsonfortran root")
-      set(JSONFORTRAN_FROM_GIT OFF)
-   elseif (DEFINED jsonfortran_ROOT)
-      set(${JSONFORTRAN_PACKAGE_NAME}_ROOT "${jsonfortran_ROOT}" CACHE STRING "jsonfortran root")
-      set(JSONFORTRAN_FROM_GIT OFF)
-   elseif (DEFINED jsonfortran_DIR)
-      set(${JSONFORTRAN_PACKAGE_NAME}_DIR "${jsonfortran_DIR}" CACHE STRING "jsonfortran dir")
-      set(JSONFORTRAN_FROM_GIT OFF)
-   endif()
+   add_library("${JSONFORTRAN_LIBRARY}" ALIAS "jsonfortran")
 
-   set(JSONFORTRAN_VERSION "9.2.1" CACHE STRING "'jsonfortran' library version")
+   set(JSONFORTRAN_MOD_DIR "${JSONFORTRAN_BINARY_DIR}/mod" CACHE STRING "" FORCE)
+   message(STATUS "*** ${JSONFORTRAN_MOD_DIR}")
 
-   if(JSONFORTRAN_FROM_GIT)
+   set_target_properties("jsonfortran"
+      PROPERTIES
+      Fortran_MODULE_DIRECTORY ${JSONFORTRAN_MOD_DIR}
+   )
 
-      set(JSONFORTRAN_GITHUB_URL "https://github.com/jacobwilliams/json-fortran.git")
-      set(JSONFORTRAN_GITHUB_TAG "${JSONFORTRAN_VERSION}")
+   target_include_directories("jsonfortran" INTERFACE
+      $<BUILD_INTERFACE:${JSONFORTRAN_MOD_DIR}>
+      $<INSTALL_INTERFACE:mod>
+   )
 
-      message(STATUS "*** Using 'FetchContent' to obtain library 'jsonfortran' from GitHub")
-      message(STATUS "*** ${JSONFORTRAN_GITHUB_URL}, version: ${JSONFORTRAN_GITHUB_TAG}")
+   include(GNUInstallDirs)
 
-      include(FetchContent)
-      set(JSONFORTRAN_ENABLE_DOC_GENERATION OFF)  # disable generation documentation
-      set(JSONFORTRAN_ENABLE_TESTS OFF)           # disable tests
-      set(JSONFORTRAN_STATIC_LIBRARY_ONLY ON)     # build only static library
-      FetchContent_Declare(
-        jsonfortran
-        GIT_REPOSITORY ${JSONFORTRAN_GITHUB_URL}
-        GIT_TAG ${JSONFORTRAN_GITHUB_TAG}
-        GIT_SHALLOW ON
-      )
-      FetchContent_MakeAvailable(jsonfortran)
-      add_library("${JSONFORTRAN_LIBRARY}" ALIAS "jsonfortran")
+   install(DIRECTORY ${JSONFORTRAN_MOD_DIR}/
+      DESTINATION mod
+      COMPONENT library
+      FILES_MATCHING PATTERN "*.mod"
+   )
 
-   else(JSONFORTRAN_FROM_GIT)
+   install(TARGETS "jsonfortran"
+      EXPORT ${PROJECT_NAME}-targets
+      ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+      LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+      RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+      COMPONENT library
+   )
 
-      # Try to find "jsonfortran" library using
-      # -Djsonfortran_DIR: path to the `*config.cmake` configuration file
-      # -Djsonfortran_ROOT and -DJSONFORTRAN_ROOT: path to the root directory where 'jsonfortran' is installed
-      # -DJSONFORTRAN_ROOT project name can be written using capital letters since CMake 3.27 (see policy CMP0144)
-
-      find_package(${JSONFORTRAN_PACKAGE_NAME} REQUIRED)
-      add_library("${JSONFORTRAN_LIBRARY}" ALIAS "${JSONFORTRAN_PACKAGE_NAME}::jsonfortran")
-
-      message(STATUS "*** Library 'jsonfortran' found: ${${JSONFORTRAN_PACKAGE_NAME}_DIR}")
-
-   endif(JSONFORTRAN_FROM_GIT)
+   # string(TOLOWER "jsonfortran-${CMAKE_Fortran_COMPILER_ID}" JSONFORTRAN_PACKAGE_NAME)
+   #
+   # set(JSONFORTRAN_FROM_GIT ON CACHE BOOL "Get 'jsonfortran' library from Github")
+   #
+   # if (DEFINED JSONFORTRAN_ROOT)
+   #    set(${JSONFORTRAN_PACKAGE_NAME}_ROOT "${JSONFORTRAN_ROOT}" CACHE STRING "jsonfortran root")
+   #    set(JSONFORTRAN_FROM_GIT OFF)
+   # elseif (DEFINED jsonfortran_ROOT)
+   #    set(${JSONFORTRAN_PACKAGE_NAME}_ROOT "${jsonfortran_ROOT}" CACHE STRING "jsonfortran root")
+   #    set(JSONFORTRAN_FROM_GIT OFF)
+   # elseif (DEFINED jsonfortran_DIR)
+   #    set(${JSONFORTRAN_PACKAGE_NAME}_DIR "${jsonfortran_DIR}" CACHE STRING "jsonfortran dir")
+   #    set(JSONFORTRAN_FROM_GIT OFF)
+   # endif()
+   #
+   # set(JSONFORTRAN_VERSION "9.2.1" CACHE STRING "'jsonfortran' library version")
+   #
+   # if(JSONFORTRAN_FROM_GIT)
+   #
+   #    set(JSONFORTRAN_GITHUB_URL "https://github.com/jacobwilliams/json-fortran.git")
+   #    set(JSONFORTRAN_GITHUB_TAG "${JSONFORTRAN_VERSION}")
+   #
+   #    message(STATUS "*** Using 'FetchContent' to obtain library 'jsonfortran' from GitHub")
+   #    message(STATUS "*** ${JSONFORTRAN_GITHUB_URL}, version: ${JSONFORTRAN_GITHUB_TAG}")
+   #
+   #    include(FetchContent)
+   #    set(JSONFORTRAN_ENABLE_DOC_GENERATION OFF)  # disable generation documentation
+   #    set(JSONFORTRAN_ENABLE_TESTS OFF)           # disable tests
+   #    set(JSONFORTRAN_STATIC_LIBRARY_ONLY ON)     # build only static library
+   #    FetchContent_Declare(
+   #      jsonfortran
+   #      GIT_REPOSITORY ${JSONFORTRAN_GITHUB_URL}
+   #      GIT_TAG ${JSONFORTRAN_GITHUB_TAG}
+   #      GIT_SHALLOW ON
+   #    )
+   #    FetchContent_MakeAvailable(jsonfortran)
+   #    add_library("${JSONFORTRAN_LIBRARY}" ALIAS "jsonfortran")
+   #
+   # else(JSONFORTRAN_FROM_GIT)
+   #
+   #    # Try to find "jsonfortran" library using
+   #    # -Djsonfortran_DIR: path to the `*config.cmake` configuration file
+   #    # -Djsonfortran_ROOT and -DJSONFORTRAN_ROOT: path to the root directory where 'jsonfortran' is installed
+   #    # -DJSONFORTRAN_ROOT project name can be written using capital letters since CMake 3.27 (see policy CMP0144)
+   #
+   #    find_package(${JSONFORTRAN_PACKAGE_NAME} REQUIRED)
+   #    add_library("${JSONFORTRAN_LIBRARY}" ALIAS "${JSONFORTRAN_PACKAGE_NAME}::jsonfortran")
+   #
+   #    message(STATUS "*** Library 'jsonfortran' found: ${${JSONFORTRAN_PACKAGE_NAME}_DIR}")
+   #
+   # endif(JSONFORTRAN_FROM_GIT)
 
 endif(NOT TARGET "${JSONFORTRAN_LIBRARY}")
