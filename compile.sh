50f40decc23edfd86b746a0925dbbbf604de6927
ncrescenzio <nicolo.crescenzio@studenti.unipd.it>
2026-04-08 14:31:03 +0200

    minor

--- compile.sh
+++ compile.sh
@@ -4,9 +4,10 @@ builddir=build
 installdir=../libglobals/
 
 [[ -d "$builddir" ]] && rm -rf "$builddir"
+[[ -d "$libglobals" ]] && rm -rf "$libglobals"
 
-cmake -S . -B "$builddir"
-cmake --build "$builddir"
+cmake -S . -B "$builddir" -DCMAKE_BUILD_TYPE="Release"
+cmake --build "$builddir" -j $(nproc)
 cmake --install "$builddir" --prefix "$installdir"
 
 
